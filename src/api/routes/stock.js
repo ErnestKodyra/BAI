65390ea524c1494911d1d3498f1563ae007d7faf
EryktheCreator <s218498@student.uek.krakow.pl>
2024-05-29 02:12:54 +0200

    fix: different trends for each stock

--- src/api/routes/stock.js
+++ src/api/routes/stock.js
@@ -9,103 +9,105 @@ let stocks = [
         { date: Date(), open: 92.86, high: 93.05, low: 91.20, close: 91.55 }
     ]}
 ];
-let tickCounter = 0;
-let tickAmount = 15;
-let trend = 'stable';
 
-setInterval(() => {
-    stocks.forEach(stock => {
-        const lastPrice = stock.prices[stock.prices.length - 1];
-        console.log(stock.symbol);
-        console.log(trend)
-        console.log(lastPrice);
-        const newPrice = generateNewPrice(lastPrice);
-        stock.prices.push(newPrice);
-        if (stock.prices.length > 500) {
-            stock.prices.shift();
+stocks.forEach(stock => {
+    let tickCounter = 0;
+    let tickAmount = 15;
+    let trend = 'stable';
+
+    setInterval(() => {
+            const lastPrice = stock.prices[stock.prices.length - 1];
+            console.log(stock.symbol);
+            console.log(trend)
+            console.log(lastPrice);
+            const newPrice = generateNewPrice(lastPrice);
+            stock.prices.push(newPrice);
+            if (stock.prices.length > 500) {
+                stock.prices.shift();
+            }
+            tickCounter++;
         }
-        tickCounter++;
-    });
-}, 1000);
+    , 1000);
 
-function generateNewPrice(lastPrice) {
-    let newPrice;
+    function generateNewPrice(lastPrice) {
+        let newPrice;
 
-    if (tickCounter % 20 === 0) {
-        let random = Math.random();
-        if (random < 0.32) {
-            trend = 'stable';
-        } else if (random < 0.50) {
-            trend =  'slightly up';
-        } else if (random < 0.68) {
-            trend =  'slightly down';
-        } else if (random < 0.80) {
-            trend =  'up';
-        } else if (random < 0.92) {
-            trend =  'down';
-        } else trend =  'volatile';
-        tickAmount = Math.round(Math.random() * 50);
+        if (tickCounter % 20 === 0) {
+            let random = Math.random();
+            if (random < 0.32) {
+                trend = 'stable';
+            } else if (random < 0.50) {
+                trend =  'slightly up';
+            } else if (random < 0.68) {
+                trend =  'slightly down';
+            } else if (random < 0.80) {
+                trend =  'up';
+            } else if (random < 0.92) {
+                trend =  'down';
+            } else trend =  'volatile';
+            tickAmount = Math.round(Math.random() * 50);
+        }
+
+        switch (trend) {
+            case 'stable':
+                let deviation = Math.random() < 0.5 ? (Math.random() * 0.2) : -(Math.random() * 0.2);
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 0.5).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 0.5).toFixed(2)),
+                close: parseFloat((lastPrice.close + deviation).toFixed(2))
+                };
+                break;
+            case 'up':
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2)),
+                close: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2))
+                };
+                break;
+            case 'down':
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2)),
+                close: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2))
+                };
+                break;
+            case 'slightly up':
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2)),
+                close: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2))
+                };
+                break;
+            case 'slightly down':
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2)),
+                close: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2))
+                };
+                break;
+            case 'volatile':
+                newPrice = {
+                date: new Date(),
+                open: parseFloat((lastPrice.close).toFixed(2)),
+                high: parseFloat((lastPrice.close + Math.random() * 6).toFixed(2)),
+                low: parseFloat((lastPrice.close - Math.random() * 6).toFixed(2)),
+                close: parseFloat((lastPrice.close + Math.random() * 6 - Math.random() * 6).toFixed(2))
+                };
+                break;
+        }
+        return newPrice;
     }
-
-    switch (trend) {
-        case 'stable':
-            let deviation = Math.random() < 0.5 ? (Math.random() * 0.2) : -(Math.random() * 0.2);
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 0.5).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 0.5).toFixed(2)),
-            close: parseFloat((lastPrice.close + deviation).toFixed(2))
-            };
-            break;
-        case 'up':
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2)),
-            close: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2))
-            };
-            break;
-        case 'down':
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 2).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2)),
-            close: parseFloat((lastPrice.close - Math.random() * 2).toFixed(2))
-            };
-            break;
-        case 'slightly up':
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2)),
-            close: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2))
-            };
-            break;
-        case 'slightly down':
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 1.25).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2)),
-            close: parseFloat((lastPrice.close - Math.random() * 1.25).toFixed(2))
-            };
-            break;
-        case 'volatile':
-            newPrice = {
-            date: new Date(),
-            open: parseFloat((lastPrice.close).toFixed(2)),
-            high: parseFloat((lastPrice.close + Math.random() * 6).toFixed(2)),
-            low: parseFloat((lastPrice.close - Math.random() * 6).toFixed(2)),
-            close: parseFloat((lastPrice.close + Math.random() * 6 - Math.random() * 6).toFixed(2))
-            };
-            break;
-    }
-    return newPrice;
-}
+});
 
 router.get('/:symbol', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*")
